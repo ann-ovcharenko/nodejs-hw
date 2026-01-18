6c3eb876591bcc1802a67cc817516d3d325b7657
Anna Ovcharenko <anyutka1426@gmail.com>
2026-01-19 00:05:01 +0200

    Fix errorHandler: simplify response structure to only message property

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -4,15 +4,11 @@ export const errorHandler = (err, req, res, next) => {
   
   if (isHttpError(err)) {
     return res.status(err.status).json({
-      status: err.status,
-      message: err.name,
-      data: err.message,
+      message: err.message,
     });
   }
 
   res.status(500).json({
-    status: 500,
     message: 'Something went wrong',
-    data: err.message,
   });
 };
